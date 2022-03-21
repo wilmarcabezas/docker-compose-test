0cf76a08c92c83cb35569c503c57fa1752d72fa2
wilmarux <wilmar.cabezas@gmail.com>
2022-03-21 00:50:51 -0500

    Release

--- build/production.Dockerfile
+++ build/production.Dockerfile
@@ -4,8 +4,6 @@ COPY ["package.json", "package-lock.json", "/usr/src/"]
 
 WORKDIR /usr/src
 
-RUN npm install --only=production
-
 COPY [".", "/usr/src/"]
 
 RUN npm install --only=development
--- index.js
+++ index.js
@@ -10,12 +10,12 @@ const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost:27017/test';
 app.get('/', (req, res) => {
   MongoClient.connect(mongoUrl, { useNewUrlParser: true }, (err, db) => {
     if (err) {
-      res.status(500).send('💥 BOOM 💥: ' + err);
+      res.status(500).send('💥 PUMP 💥: ' + err);
     } else {
-      res.send('Me conecté a la DB! 😎');
+      res.send('Me conecté a la DB!!!! 😎');
       db.close();
     }
   });
 });
 
-app.listen(port, () => console.log(`Server listening on port ${port}!`))
+app.listen(port, () => console.log(`Server listening... on port ${port}!`))
